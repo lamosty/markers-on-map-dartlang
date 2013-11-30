3a23c9cbc19e596e23b0cea5cdd8e212b08dfdc9
Rastislav Lamos <lamos.rasto@gmail.com>
2013-11-30 10:24:35 +0100

    rails scaffold

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Mapengine::Application.config.secret_key_base = '8bda446087885d226a07cd8efd09b58c9cb3630bdd4dfb418bc375373eaa318af13e2f2142b1dc2714912b34737042ee622b40d165abd99cc8551a9db8f13540'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Mapengine::Application.config.session_store :cookie_store, key: '_mapengine_session'
