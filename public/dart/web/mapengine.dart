a17fcb625eb007d905b36654f2e0f5c25c523d44
Rastislav Lamos <lamos.rasto@gmail.com>
2013-12-05 15:04:35 +0100

    added gmap3 plugin and some js files

--- public/dart/web/mapengine.dart
+++ public/dart/web/mapengine.dart
@@ -1,6 +1,28 @@
 import 'dart:html';
+import 'dart:js';
 
 void main() {
-  querySelector('#map')..text = "hehe";
+  var object = new JsObject(context['Object']);
+  object['greeting'] = 'Hello';
+  object['greet'] = (name) {
+    return "${object['greeting']} $name";
+  };
+
+  var jsMap = new JsObject.jsify([1, 2, 3]);
+
+  var result = test(jsMap);
+
+  context.callMethod('alert', [result]);
 }
+
+dynamic test(f) {
+  var result = 0;
+
+  for (var i = 0; i < f.length; i++) {
+    result += f[i];
+  }
+
+  return result;
+}
+
 
