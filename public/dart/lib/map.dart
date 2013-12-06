e3c7f2771205c49934132bcc277fdc518382e248
Rastislav Lamos <lamos.rasto@gmail.com>
2013-12-06 12:02:01 +0100

    adding basic functionality to the map library

--- public/dart/lib/map.dart
+++ public/dart/lib/map.dart
@@ -4,14 +4,45 @@ import 'dart:js';
 
 class Map {
   final String elementId;
+  var mapOptions;
   
-  Map(
-      this.elementId
-  );
+  // JS contexts
+  final jQuery = context['jQuery'];
+  final gmaps = context['google']['maps']; 
   
-  void gmap3(options) {
-    var jQuery = context['\$'];
-    jQuery.apply([this.elementId]).callMethod('gmap3', [options]);
-
+  // Gmap3 plugin name
+  final String gmap3 = 'gmap3';
+  
+  Map(this.elementId) {
+    mapOptions = {
+      'center': [48.161154, 17.137031],
+      'mapTypeId': gmaps['MapTypeId']['ROADMAP'],
+      'zoom': 15
+    };
   }
+
+  void setMapOptions({
+    List<double> center
+  }) {
+    mapOptions['center'] = center;
+  }
+  
+  JsObject _jsifyMapOptions() {
+    return new JsObject.jsify({
+      'map': {
+        'options': mapOptions
+      }
+    });
+  }
+  
+  void drawMap() {
+    jQuery.apply([elementId]).callMethod(gmap3, [this._jsifyMapOptions()]);
+  }
+  
+  void autofitMap() {
+    var applied = jQuery.apply([elementId]);
+    
+    applied.callMethod(gmap3, ['autofit']);
+  }
+  
 }
--- public/dart/web/mapengine.dart
+++ public/dart/web/mapengine.dart
@@ -2,23 +2,10 @@ import 'dart:html';
 import 'dart:js';
 import 'package:mapengine/map.dart' as GMap;
 
-void main() {
-  var jQuery = context['\$'];
-  var googleMaps = context['google']['maps'];
-  var mapOptions = new JsObject.jsify({
-      'map': {
-        'address':"POURRIERES, FRANCE",
-        'options':{
-          'zoom':10,
-          'mapTypeId': googleMaps['MapTypeId']['ROADMAP'],
-          'mapTypeControl': true
-        }
-      }
-    });
-  
-  
+void main() {  
   var map = new GMap.Map('#map');
-  map.gmap3(mapOptions);
+  map.drawMap();
+  map.autofitMap();
 }
 
 
